f67dece0289c45f0b13d5075c70a3d7c6ec35d74
David Chen <mvjome@gmail.com>
2015-07-24 17:25:59 +0800

    Extract Steps from RsyncRun

--- rsyncrun/rsync_run.py
+++ rsyncrun/rsync_run.py
@@ -13,22 +13,17 @@ from .json_conf_template import JsonConfTemplate
 from .compatible import Compatible
 from .shell import Shell
 from .installer import Installer
+from .steps import Steps
 
 # TODO mock remote server
 
 
-class RsyncRun(object):
+class RsyncRun(Steps):
+    """
+    RsyncRun configration module.
 
-    ordered_steps_list = [
-        "setup_conf",
-        "validate",
-        "sync_source_code",
-        "prepare_virtualenv",
-        "install_package_lazily",
-        "run_some_before_scripts",
-        "launch_program",
-        "clean",
-    ]
+    Executable code are placed in Steps.
+    """
 
     def __init__(self, sys_argv=[]):
         self.argv = sys_argv
@@ -104,88 +99,6 @@ class RsyncRun(object):
     def rsync_output_file_remote(self):
         return "%s_2" % self.rsync_output_file
 
-    def setup_conf(self):
-        if not os.path.exists(self.conf_file):
-            print """[warn] can't find %s ! Please create one, e.g. %s""" % (
-                self.conf_file, JsonConfTemplate.example)
-            exit(1)
-
-    def validate(self):
-        assert isinstance(self.conf, dict)
-        invalid_example_info = "See valid example in conf %s" % JsonConfTemplate.example
-        assert "sync_projects" in self.conf, invalid_example_info
-        assert "remote_server" in self.conf, invalid_example_info
-        conf_file_msg = "Auto detected" if self.guess_conf_file is self.conf_file else "Specified"
-        print "** %s config file is %s" % (conf_file_msg, self.conf_file)
-
     @cached_property
     def remote_user_host(self):
         return "%s@%s" % (self.conf["remote_server"]["user"], self.conf["remote_server"]["host"],)
-
-    def sync_source_code(self):
-        for sync_opts_type in ["local_to_remote", "remote_to_remote"]:
-            sync_opt2 = self.conf["sync_projects"][sync_opts_type]
-            for project1 in sync_opt2:
-
-                if sync_opts_type == "remote_to_remote":
-                    from_addr, to_addr = sync_opt2[project1]
-                else:
-                    from_addr = sync_opt2[project1][0]
-                    to_addr = "%s:%s" % (self.remote_user_host, sync_opt2[project1][1])
-                source_code_sync_command2 = """%s %s  %s | tee -a %s """ % \
-                    (self.rsync_cmd,
-                     from_addr,
-                     to_addr,
-                     self.rsync_output_file)
-
-                if sync_opts_type == "remote_to_remote":
-                    self.shell.remote(source_code_sync_command2)
-                else:
-                    self.shell.local(source_code_sync_command2)
-
-        # get remote changed content
-        self.shell.local("""scp %s:%s %s; cat %s >> %s; rm -f %s; """ % (
-            self.remote_user_host,
-            self.rsync_output_file,
-            self.rsync_output_file_remote,
-            self.rsync_output_file_remote,
-            self.rsync_output_file,
-            self.rsync_output_file_remote))
-        self.source_code_sync_result = file(self.rsync_output_file).read()
-
-        self.shell.local("echo rsync_output_file; cat %s" % self.rsync_output_file)
-        self.shell.local("rm -f %s" % self.rsync_output_file)
-
-    def prepare_virtualenv(self):
-        self.shell.remote("""
-        if [ ! -f ENV/bin/activate ]; then
-            pip install virtualenv
-            virtualenv ENV
-        fi;
-        """)
-
-    def install_package_lazily(self):
-        self.installer.run(self.source_code_sync_result)
-
-    def run_some_before_scripts(self):
-        scripts = self.conf.get("scripts_before_run", list())
-        scripts = map(lambda s1: s1.strip(), scripts)
-        # Ignore comment.
-        scripts = filter(lambda s1: s1.startswith("#"), scripts)
-
-        for script1 in scripts:
-            self.shell.remote(script1)
-
-    def launch_program(self):
-        task_opts = self.conf["launch"].get("params_list", list())
-        if not isinstance(task_opts, list):
-            task_opts = [task_opts]
-        params_index = self.conf["launch"].get("params_index", 0)
-        task_opts = task_opts[params_index]
-        self.shell.remote(self.conf["launch"]["template"] % tuple(task_opts))
-
-    def clean(self):
-        """ when exit, clean """
-        clean_file_under_root_tmp = """
-            find /tmp/ -maxdepth 1 -type f -mmin +30 | grep %s | xargs rm -f ;"""
-        self.shell.remote(clean_file_under_root_tmp % JsonConfTemplate.name_prefix)
